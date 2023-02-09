3a0fe19ce130b5bcf314e97515716ab6ea56fa95
Sasha Sasha <sashamaninaa@mail.ru>
2023-02-10 01:43:54 +0300

    add clients block

---
+++ assets/scripts/slider.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        infinite: false,
+        slidesToShow: 4,
+        speed: 600,
+        easing: 'ease',
+        cssEase: 'ease-in',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 481,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
