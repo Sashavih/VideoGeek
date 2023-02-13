ba2cc240a2eee91529ea148145d5c88cdcba7621
Sasha Sasha <sashamaninaa@mail.ru>
2023-02-13 23:28:00 +0300

    сделала размер шрифта как на сайте

--- assets/scripts/mainBlock.js
+++ assets/scripts/mainBlock.js
@@ -1,7 +1,7 @@
 $(function () {
     $('#mainTitle').fadeIn(2400);
     $('#subtitleAnimation').fadeIn(2800);
-    $('.avatar').fadeIn(2400);
+    $('.avatar').fadeIn(2300);
     $('.mainBlock__btn').fadeIn(3200);
 
 })
