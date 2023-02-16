2f0b1e63be88d435be12d8ec3d23d5447015c1b1
Sasha Sasha <sashamaninaa@mail.ru>
2023-02-17 02:10:30 +0300

    сделала юлок плюсы+анимация

--- assets/scripts/mainBlock.js
+++ assets/scripts/mainBlock.js
@@ -4,5 +4,8 @@ $(function () {
     $('.avatar').fadeIn(2300);
     $('.mainBlock__btn').fadeIn(2000);
     $('.header').fadeIn(3200);
-
+    $('.plusesBox__item1').fadeIn(900);
+    $('.plusesBox__item2').fadeIn(1000);
+    $('.plusesBox__item3').fadeIn(1100);
+    $('.plusesBox__item4').fadeIn(1200);
 })
