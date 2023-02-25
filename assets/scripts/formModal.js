ab0d9195e23419e2b2305eb53aaa2fa0a235a5be
Sasha Sasha <sashamaninaa@mail.ru>
2023-02-25 13:27:28 +0300

    .

--- assets/scripts/formModal.js
+++ assets/scripts/formModal.js
@@ -1,10 +1,18 @@
 let modal = $modal({
-    content: `<h2 class="subtitle ">отправить тз</h2><form class="form">
-    <input type="text" name="text" class="form__item" />
-    <input type="tel" name="tel" class="form__item" />
-    <input type="email" name="email" class="form__item" />
-    <input type="file" name="file">
-    <textarea rows="5" class="form__item" name="comment"></textarea>
+    content: `<h2 class="subtitle ">отправить тз</h2>
+    <form class="form">
+    <input type="text" name="text" class="form__item" placeholder="Ваше Имя" required/>
+    <input type="email" name="email" class="form__item" placeholder="E-mail"  required/>
+    <input type="tel" name="tel" class="form__item" id="phone" placeholder="Номер телефона" minlength="11" maxlength="12"required/>
+
+    <div class="file-upload">
+	<div class="file-upload-select">
+		<div class="file-select-button btn btn-form" >Выберите файл</div>
+    <div class="file-select-name">Файл не выбран...</div> 
+    <input type="file" name="file-upload-input" id="file-upload-input" >
+	</div>
+</div>
+    <textarea rows="5" class="form__item" name="comment" placeholder="Комментарий"></textarea>
     <button type="submit" class="btn btn-form">отправить</button>
     <p>Нажимая кнопку «Отправить», вы даете согласие с нашей Политикой обработки персональных данных</p>
 </form>`,
@@ -16,4 +24,22 @@ document.addEventListener('click', function (e) {
     if (e.target.dataset.toggle === 'modal') {
         modal.show();
     }
+});
+let form = document.querySelector('.form');
+let phone = document.querySelector('#phone');
+
+form.addEventListener('submit', function (evt) {
+    console.log(phone.value);
+    evt.preventDefault();
+    fetch('https://httpbin.org/post', {
+        method: 'POST',
+        body: new FormData(form)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => console.log(error));
+
+    alert('Ваша заявка успешно отправлена!');
 });
